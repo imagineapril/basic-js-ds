f827393c178cc3fb7ad15f775a1d7ddfe0cf543e
imagineapril <julia.timokhina604@gmail.com>
2025-01-06 00:06:57 +0300

    implement some BTS parts

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,25 +7,49 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor()  {
+    this.data = null;
+    this.left = null;
+    this.right = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.data === null)  {
+      return null;
+    } else {
+      return this;
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.data === null)  {
+      this.data = data;
+    } else {
+      if (data < this.data)  {
+        if (this.left === null)  {
+        this.left = new BinarySearchTree();
+        this.left.add (data);
+        }
+      }
+      if (data > this.data)  {
+        if (this.right === null)  {
+          this.right = new BinarySearchTree();
+          this.right.add(data);
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) !== null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (this.data === null) return null;
+    if (data === this.data) return this;
+    if (data < this.data && this.left) return this.left.find(data);
+    if (data > this.data && this.right) return this.right.find(data);
+    return null;
   }
 
   remove(/* data */) {
